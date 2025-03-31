b52295f487ed195b7ca1fa931c7a10f6b6788776
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2025-03-31 17:06:47 -0300

    feat(if-else): adiciona verificação de ano bissexto

--- If_Else/anobissexto.js
+++ If_Else/anobissexto.js
@@ -3,5 +3,5 @@ let ano = 2024;
 if ((ano % 4 == 0 && ano % 100 !== 0) || (ano % 400 == 0))
 {console.log(`O ano ${ano} é bissexto!`)}
 else
-{console.log(`O ano ${ano} não é bissexto.`)}
+{console.log(`O ano ${ano} não é bissexto`)}
 
